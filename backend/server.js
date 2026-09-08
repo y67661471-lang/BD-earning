c1f48bc0a25bbc29c95617308905ad26d9759106
y67661471-lang <y67661471@gmail.com>
2026-09-08 19:49:27 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/app", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
